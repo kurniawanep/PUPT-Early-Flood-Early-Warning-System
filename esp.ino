260dc073d9df14ba5f5314651ef08b5e4440f7c7
Kurniawan Eko Putra <58814910+kurniawanep@users.noreply.github.com>
2023-09-14 02:39:45 +0700

    Update esp.ino

--- esp.ino
+++ esp.ino
@@ -1,32 +1,48 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
 SoftwareSerial arduino(D6,D7);
 //ArduinoJson
 #include <ArduinoJson.h>
+#include <UniversalTelegramBot.h>
+#include <WiFiClientSecure.h>
 
-WiFiClient client;
+#define BOTtoken "6356213264:AAGdqdboiQabCL-bqmBCo1ncwSU49KqEYEI"
+#define CHAT_ID "-4004373256"
+
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
 const IPAddress server(0,0,0,0);
 const int httpPort = 80;
 const int BUFFER_SIZE = JSON_OBJECT_SIZE(2048);
-const char* ssid = "Dosen Spot";
-const char* password = "stmkgcollege";
+// const char* ssid = "STMKG Spot";
+// const char* password = "stmkg2014";
+const char* ssid = "BOLT!Super4G-29C2";
+const char* password = "eqy04890";
 
 void setup () {
   arduino.begin(9600);
   Serial.begin(9600);
-   WiFi.mode(WIFI_STA);
+  configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
+  client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+  // WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
     int i=0;
     while(WiFi.status() != WL_CONNECTED){ 
       Serial.print(".");
       delay(1000);     
-    } 
+    }
+   
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+  bot.sendMessage(CHAT_ID, "Wifi Terhubung!", "");
+  bot.sendMessage(CHAT_ID, "System dimulai!!", "");
   delay(2000); 
 }
 
@@ -41,42 +57,59 @@ void loop () {
 
   
     int level = doc["a"];
-    int hujan = doc["b"];
+    // int hujan = doc["b"];
     Serial.print("Received data: ");
     Serial.println(level);
-     if (isnan(level) || isnan(hujan)) {
+     if (isnan(level)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
      }
     if ((WiFi.status() == WL_CONNECTED)) {
-    WiFiClient client;
-    HTTPClient http;
-    
-    String address;
-    //equate with your computer's IP address and your directory application
-    // C:\xampp\htdocs\arducoding_tutorial\nodemcu_log\webapi\api\create.php
-    address ="http://10.53.0.162/banjir/webapi/api/create.php?level=";
-    address += String(level);
-    address += "&hujan="; 
-    address += String(hujan) ;
+      if(level < 300 && level >= 250) {
+        String message = "Status Banjir : SIAGA\nTinggi air melampaui batas, saat ini tinggi air: " + String(level) + " cm";
+        bot.sendMessage(CHAT_ID, message, "");
+      }
+      if (level < 250 && level >= 200) {
+        String message = "Status Banjir : WASPADA\nTinggi air melampaui batas, saat ini tinggi air: " + String(level) + " cm";
+        bot.sendMessage(CHAT_ID, message, "");
+      }
+      if (level < 200){
+        String message = "Status Banjir : AWAS\nTinggi air melampaui batas, saat ini tinggi air: " + String(level) + " cm";
+        // sendMessage(telegramGroupId, message);
+        // Wait 5 seconds before sending another message (adjust as needed)
+        bot.sendMessage(CHAT_ID, message, "");
+      }
+      std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+      client->setInsecure();
       
-    http.begin(client,address);  //Specify request destination
-    int httpCode = http.GET();//Send the request
-    String payload;  
-    if (httpCode > 0) { //Check the returning code    
-        payload = http.getString();   //Get the request response payload
-        payload.trim();
-        if( payload.length() > 0 ){
-           Serial.println(payload + "\n");
-        }
-    }
-    
-    http.end();   //Close connection
+      //create an HTTPClient instance
+      HTTPClient https;
+      
+      String address;
+      //equate with your computer's IP address and your directory application
+      // C:\xampp\htdocs\arducoding_tutorial\nodemcu_log\webapi\api\create.php
+      address ="https://hanip.my.id/webapi/api/create.php?level=";
+      address += String(level);
+      address += "&hujan=0"; 
+      // address += String(hujan) ;
+        
+      https.begin(*client,address);  //Specify request destination
+      int httpCode = https.GET();//Send the request
+      String payload;  
+      if (httpCode > 0) { //Check the returning code    
+          payload = https.getString();   //Get the request response payload
+          payload.trim();
+          if( payload.length() > 0 ){
+            Serial.println(payload + "\n");
+          }
+      }
+      
+      https.end();   //Close connection
   }else{
     Serial.print("Not connected to wifi ");Serial.println(ssid);
   }
-  delay(2000);
-
+  delay(4000);
+  
   }
   
 }
